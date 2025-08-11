ec112009150272011487c9a1705586f97484ea67
NOBITA <nobitaanetwork@gmail.com>
2025-08-11 14:13:27 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageEnhance
 from PIL import ImageFilter, ImageFont, ImageOps
 
 from unidecode import unidecode
-from youtubesearchpython.future import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 from AnonXMusic import app
 from config import YOUTUBE_IMG_URL
